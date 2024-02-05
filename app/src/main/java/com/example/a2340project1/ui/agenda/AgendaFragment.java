fe854d29b40e50e67b941012186a9c9d4ea4f4db
Aidan Nguyen <aidannguyen@lawn-143-215-102-255.lawn.gatech.edu>
2024-02-05 18:17:34 -0500

    Added spinners for sorting on agenda fragment

--- app/src/main/java/com/example/a2340project1/ui/agenda/AgendaFragment.java
+++ app/src/main/java/com/example/a2340project1/ui/agenda/AgendaFragment.java
@@ -10,10 +10,12 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,9 +25,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.a2340project1.R;
 import com.example.a2340project1.databinding.FragmentAgendaBinding;
+import com.example.a2340project1.ui.classes.ClassElementHandler;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Objects;
@@ -36,6 +40,11 @@ public class AgendaFragment extends Fragment implements DatePickerDialog.OnDateS
     private final AgendaElementHandler ELEMENT_HANDLER = new AgendaElementHandler();
 
     private LinearLayout layoutList;
+    private Spinner dateOrClassSort;
+    private Spinner examOrAssignmentSort;
+
+    private ArrayList<String> dateOrClassSortList = new ArrayList<>();
+    private ArrayList<String> examOrAssignmentSortList = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -46,6 +55,26 @@ public class AgendaFragment extends Fragment implements DatePickerDialog.OnDateS
         View root = binding.getRoot();
 
         layoutList = root.findViewById(R.id.agenda_layout);
+        dateOrClassSort = root.findViewById(R.id.date_class_sort);
+        examOrAssignmentSort = root.findViewById(R.id.exam_assignment_sort);
+
+        examOrAssignmentSortList.add("Sort by all");
+        examOrAssignmentSortList.add("Sort by exam");
+        examOrAssignmentSortList.add("Sort by assignment");
+
+        dateOrClassSortList.add("Sort by date");
+        dateOrClassSortList.add("Sort by class");
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this.getContext(),
+                android.R.layout.simple_spinner_dropdown_item, dateOrClassSortList);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dateOrClassSort.setAdapter(adapter1);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this.getContext(),
+                android.R.layout.simple_spinner_dropdown_item, examOrAssignmentSortList);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        examOrAssignmentSort.setAdapter(adapter2);
+
 
         return root;
     }
