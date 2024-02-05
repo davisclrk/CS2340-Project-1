c28c26b0e963e664c037ee283bb1eed508d2b304
Aidan Nguyen <aidannguyen@lawn-143-215-102-255.lawn.gatech.edu>
2024-02-04 19:54:46 -0500

    Added parser to add date and time upon adding a new assignment

--- app/src/main/java/com/example/a2340project1/ui/agenda/AssignmentElement.java
+++ app/src/main/java/com/example/a2340project1/ui/agenda/AssignmentElement.java
@@ -2,7 +2,9 @@ package com.example.a2340project1.ui.agenda;
 
 public class AssignmentElement extends AgendaElement {
 
-    public AssignmentElement(int mainResource, String agendaName, int agendaMonth, int agendaDay, int agendaYear, int agendaHour, int agendaMinute, String agendaClass) {
-        super(mainResource, agendaName, agendaMonth, agendaDay, agendaYear, agendaHour, agendaMinute, agendaClass);
+    public AssignmentElement(int mainResource, String agendaName, String agendaClass, String agendaDate,
+                             int agendaMonth, int agendaDay, int agendaYear, int agendaHour, int agendaMinute) {
+        super(mainResource, agendaName, agendaClass, agendaDate, agendaMonth, agendaHour, agendaMinute,
+                agendaDay, agendaYear);
     }
 }
